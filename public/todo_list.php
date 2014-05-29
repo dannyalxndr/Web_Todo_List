990f37bc1c07cc454b50ca5d50a52953ebb8d69d
Daniel Jimenez <danieljimenez@Daniels-MacBook-Air.local>
2014-05-29 14:08:36 -0500

    created an add item button to add to the list, and are able to save the file

--- public/todo_list.php
+++ public/todo_list.php
@@ -8,8 +8,11 @@
 		
 		var_dump($_GET);
 		var_dump($_POST);
+
+		// load the file
 		$items = ["Take out trash", "Go to the grocery store", "write a better todo list"];
-		$filename = "data/groceries.txt";
+		$filename = "data/list.txt";
+		$new_items = open_file($filename);
 
 /////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
@@ -24,32 +27,60 @@
 		    return $contents_array;
 		}
 
-		$new_items = open_file($filename);
+		function save_file($filename, $data_to_save)
+		{
+            $handle = fopen($filename, 'w');
+            $contents = implode("\n", $data_to_save);
+            fwrite($handle, $contents);
+            fclose($handle);
+		}
 
-	
+		if(!empty($_POST))
+		{
+			$new_items[] = $_POST['new_item'];	
+		}
+
 	?>
-
 
 	<h3>TODO List</h3>
 
-	<?php		
-		foreach($items as $index => $value)
+	<?php	
+
+		foreach($items as $item)
 		{
-			echo "<ul><li>$value</li></ul>";
+			echo "<ul><li>$item</li></ul>";
 		}
-		foreach($new_items as $index => $value)
-		{
+		foreach($new_items as $value)
+		{	
 			echo "<ul><li>$value</li></ul>";
 		}	
+		save_file($filename, $new_items);
+
 	?>
 
 	<form method="POST">
 		<label for="new_item">Add TODO Item</label><br>
 		<input type="text" name="new_item" id="new_item" placeholder="Add New Item">
 	
-		<button type="submit" name="submit">Submit</button>
-		
+		<button type="submit" name="submit">Submit</button>	
 	</form>
 
 </body>
 </html>
+
+<!-- var_dump($_GET);
+
+// sample_post.php?action=remove&index=0 -->
+
+
+
+
+
+
+
+
+
+
+
+
+
